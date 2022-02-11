10f0028a78a7d35b921d93a120910a1899810e54
Xyphyn <xyphyn@outlook.com>
2022-02-11 11:42:29 -0800

    addde /listrole stuff

---
+++ src/commands/listrolemembers.ts
@@ -0,0 +1,34 @@
+import { ICommand } from "wokcommands";
+import { Constants, MessageEmbed, Role } from "discord.js";
+
+export default {
+    category: 'Utility',
+    description: 'Gets all users of a role.',
+
+    slash: true,
+    testOnly: true,
+
+    options: [
+        {
+            name: 'role',
+            description: 'The role to get users of.',
+            required: true,
+            type: Constants.ApplicationCommandOptionTypes.ROLE
+        },
+    ],
+    
+    callback: async ({ interaction }) => {
+        interaction.deferReply()
+        const role = interaction.options.getRole('role')!
+        let embed = new MessageEmbed().setTitle(`Users of ${role.name}`)
+        const membersWithRole = interaction.guild?.roles.cache.get(role.id)?.members
+        let arr = ""
+        for (let member in membersWithRole?.values) {
+            arr += (`**${member}** `)
+        }
+        embed.setDescription(arr)
+        interaction.reply({
+            embeds: [embed]
+        })
+    }
+} as ICommand
